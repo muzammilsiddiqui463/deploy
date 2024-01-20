7f8dc946b4fe3d6dec809066fed8954fd9ce471b
Work <Work@DESKTOP-8A6U3T6.(none)>
2024-01-20 20:52:19 +0500

    Test file for server

---
+++ test.py
@@ -0,0 +1,19 @@
+from selenium.webdriver.chrome.options import Options
+import undetected_chromedriver as uc
+import os
+
+# Define Chrome options
+chrome_options = Options()
+# chrome_options.add_argument('--headless')  # Run Chrome in headless mode
+# chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+# driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH")
+
+# Initialize the WebDriver with Chrome options
+driver = uc.Chrome(options=chrome_options)
+driver.maximize_window()
+driver.get("https://github.com")
+driver.close()
+print("done")
