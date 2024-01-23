188f93a88ea4591b0764f814aa0c5c0b4973f492
Work <Work@DESKTOP-8A6U3T6.(none)>
2024-01-24 02:50:59 +0500

    Updates

--- main.py
+++ main.py
@@ -481,7 +481,7 @@ def main(csv_file_path):
         options.add_argument("--disable-gpu")
 
         # options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        options.add_experimental_option("useAutomationExtension", False)
+        # options.add_experimental_option("useAutomationExtension", False)
         options.page_load_strategy = 'eager'
 
         service_ags = ['--verbose', f'--log-path={os.getcwd()}/chromedriver.log']
